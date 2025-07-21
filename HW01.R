85b32e37c80b0b09097c51640384b75d5167c52a
Johnny Bachrach <jrbachrach@gmail.com>
2025-07-20 21:32:10 -0400

    answer question 1

--- HW01.R
+++ HW01.R
@@ -1,8 +1,25 @@
 install.packages("pacman")
+library(tidyverse)
+library(lubridate)
+
 
 pacman::p_load(pacman, tidyverse, nycflights13)
 
 (log(32) * log(32)) / (11 ^ (1/3))
 
 27^(1/3)
-fdsa 
+
+dates <- seq(as.Date("2015/1/1"), as.Date("2025/1/1"), "2 months") #creates a vector with dates
+
+dates_df <- tibble(date = dates) #converts vector into date frame
+
+dates_df <- dates_df %>% #adds new colums to the date frame for year, quarter, and isoweek
+  mutate(year = year(date),
+         quarter = quarter(date),
+         week = isoweek(date))
+
+dates_df
+
+?possibly
+?parse_date_time
+?diff
